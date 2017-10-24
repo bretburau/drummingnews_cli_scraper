d304bf6eb6df4de56dc60361ff41d24eeb3404d7
bretburau <github email address>
2017-10-24 07:34:37 -0500

    Need to get magazine to 'have many' articles

--- lib/drummingnews_cli_scraper/scraper.rb
+++ lib/drummingnews_cli_scraper/scraper.rb
@@ -29,20 +29,15 @@ class DrummingNews::Scraper
 
   def scrape_modern_drummer
     list = []
-    # if !@magazines.find {|e| e.name == "Modern Drummer"}
-    #   magazine = DrummingNews::Magazine.create("Modern Drummer")
-    #   @magazines << magazine
-    # end
-    ###Scrape articles
     10.times do
       article = DrummingNews::Article.new
       article.title = "Cool article"
       article.author = "Writer Guy"
       article.date = "12-22-2017"
-      article.magazine = magazine #Assign modern drummer object to the article
+      article.magazine = DrummingNews::Magazine.all.find {|e| e.name == "Modern Drummer"} #Assign modern drummer object to the article
       list << article
     end
-
+    binding.pry
     list
   end
 
